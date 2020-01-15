0182bcd5e6c8bc55591c361a943e5e5bd0cb2d69
Shayla White <shayladwhite1@gmail.com>
2020-01-15 04:57:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,10 +11,10 @@ end
 
  def self.create_table
     sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS students ()
+      CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
-        album TEXT
+         TEXT
         )
         SQL
     DB[:conn].execute(sql) 
